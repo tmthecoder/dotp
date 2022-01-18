94f2e8433034e57741e09f604a9803322d16e96d
Tejas Mehta <tmthecoder@gmail.com>
2022-01-18 12:44:52 -0500

    otp generates on macos as well!

--- dotp_native/macos/Classes/DotpNativePlugin.swift
+++ dotp_native/macos/Classes/DotpNativePlugin.swift
@@ -6,12 +6,12 @@ public class DotpNativePlugin: NSObject, FlutterPlugin {
     let channel = FlutterMethodChannel(name: "dotp_native", binaryMessenger: registrar.messenger)
     let instance = DotpNativePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    get_current_totp_from_utf8("", 0);
+    get_current_totp_from_base32("", 0);
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
     default:
       result(FlutterMethodNotImplemented)
     }
